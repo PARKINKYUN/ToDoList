fa19444f6f33c463805cf47cdd508e935e9593ed
Justin Inkyun Park <sefthia@gmail.com>
2023-01-17 13:20:46 +0900

    init setting

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+	"todolist/app"
 
+	"github.com/urfave/negroni"
+)
+
+func main() {
+	mux := app.MakeHandler()
+	n := negroni.Classic()
+	n.UseHandler(mux)
+
+	log.Println("App started")
+	err := http.ListenAndServe(":3000", n)
+	if err != nil {
+		panic(err)
+	}
 }
