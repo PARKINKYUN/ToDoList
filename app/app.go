ef66efb550ca7e90404ebd14ffa87c5a4ab93882
Justin Inkyun Park <sefthia@gmail.com>
2023-01-20 23:50:43 +0900

    Oauth 2.0

--- app/app.go
+++ app/app.go
@@ -2,18 +2,36 @@ package app
 
 import (
 	"net/http"
+	"os"
 	"strconv"
+	"strings"
 	"todolist/model"
 
 	"github.com/gorilla/mux"
+	"github.com/gorilla/sessions"
 	"github.com/unrolled/render"
+	"github.com/urfave/negroni"
 )
 
 var rd *render.Render = render.New()
+var store = sessions.NewCookieStore([]byte(os.Getenv("SESSION_KEY")))
 
 type AppHandler struct {
 	http.Handler
 	db model.DBHandler
+}
+
+func getSessionID(r *http.Request) string {
+	session, err := store.Get(r, "session")
+	if err != nil {
+		return ""
+	}
+
+	val := session.Values["id"]
+	if val == nil {
+		return ""
+	}
+	return val.(string)
 }
 
 func (a *AppHandler) indexHandler(w http.ResponseWriter, r *http.Request) {
@@ -62,10 +80,30 @@ func (a *AppHandler) Close() {
 	a.db.Close()
 }
 
+func CheckSignin(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+	// 사용자가 요청한 url 이 로그인 화면인 경우에는 당연히 로그인이 안되어 있으므로 예외처리해야 한다.
+	if strings.Contains(r.URL.Path, "/signin.html") ||
+		strings.Contains(r.URL.Path, "/auth/google") {
+		next(w, r)
+		return
+	}
+	// 사용자가 로그인을 한 경우 "/" 로 보낸다.
+	sessionID := getSessionID(r)
+	if sessionID != "" {
+		next(w, r)
+		return
+	}
+	// 사용자가 로그인을 안한 경우 로그인 화면으로 보낸다.
+	http.Redirect(w, r, "/signin.html", http.StatusTemporaryRedirect)
+}
+
 func MakeHandler(filepath string) *AppHandler {
 	router := mux.NewRouter()
+	n := negroni.New(negroni.NewRecovery(), negroni.NewLogger(), negroni.HandlerFunc(CheckSignin), negroni.NewStatic(http.Dir("public")))
+	n.UseHandler(router)
+
 	a := &AppHandler{
-		Handler: router,
+		Handler: n,
 		db:      model.NewDBHandler(filepath),
 	}
 
@@ -73,6 +111,8 @@ func MakeHandler(filepath string) *AppHandler {
 	router.HandleFunc("/todos", a.addTodoHandler).Methods("POST")
 	router.HandleFunc("/todos/{id:[0-9]+}", a.removeTodoHandler).Methods("DELETE")
 	router.HandleFunc("/complete-todo/{id:[0-9]+}", a.completeTodoHandler).Methods("GET")
+	router.HandleFunc("/auth/google/login", googleLoginHandler)
+	router.HandleFunc("/auth/google/callback", googleAuthCallback)
 	router.HandleFunc("/", a.indexHandler)
 
 	return a
--- main.go
+++ main.go
@@ -4,18 +4,14 @@ import (
 	"log"
 	"net/http"
 	"todolist/app"
-
-	"github.com/urfave/negroni"
 )
 
 func main() {
 	mux := app.MakeHandler("./test.db") // flag.args 로 변환 가능함
 	defer mux.Close()
-	n := negroni.Classic()
-	n.UseHandler(mux)
 
 	log.Println("App started")
-	err := http.ListenAndServe(":3000", n)
+	err := http.ListenAndServe(":3000", mux)
 	if err != nil {
 		panic(err)
 	}
